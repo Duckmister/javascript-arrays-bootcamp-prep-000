e3712f0818b596e961b138bc99eccf6980e46216
T Hinder <theresa.hindersinn@gmail.com>
2017-08-30 18:32:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element)={
+function addElementToBeginningOfArray(array, element)= {
   return [element, ...array]
 }
 
-function destrucivelyAddElementToBeginningOfArray(array, element)={
-  array.push(element);
+function destrucivelyAddElementToBeginningOfArray(array, element)= {
+  array.unshift(element);
   return array
 }
